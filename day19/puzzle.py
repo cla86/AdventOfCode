7c87504fae2432a4eb2e83ee904c6e2bb7bb5218
Dawid Węgliński <dawid.weglinski@gmail.com>
2024-12-29 09:38:12 +0100

    Resolve part2, but this probably needs some optimization

--- day19/puzzle.py
+++ day19/puzzle.py
@@ -16,11 +16,28 @@ def valid_design(position, pattern, available):
                 return 1
     return 0
 
-
+# part 1
 composable = []
 for pattern in desired:
     res = valid_design(0, pattern, available)
     composable.append(res)
 
-# part 1
 print(sum(composable))
+
+# part 2
+def valid_ways(position, pattern):
+  if position >= len(pattern):
+    return 1
+  
+  ways = 0
+  for towel in available:
+    next_pos = position + len(towel)
+    if pattern[position:next_pos] == towel:
+      ways += valid_ways(next_pos, pattern)
+  return ways
+
+combinations = 0
+for pattern in desired:
+  combinations += valid_ways(0, pattern)
+
+print(combinations)
