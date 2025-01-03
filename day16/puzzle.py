17f1359fba0ef2db2bf44cc383d396f0d23cebca
Dawid Węgliński <dawid.weglinski@cloudware.pl>
2025-01-03 09:33:12 +0100

    Change first direction to solve production input

--- day16/puzzle.py
+++ day16/puzzle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import heapq
 
-with open('input.txt.mini', 'r') as f:
+with open('input.txt', 'r') as f:
     maze = f.readlines()
     maze = [list(x.strip()) for x in maze]
 
@@ -11,34 +11,28 @@ for i in range(len(maze)):
     for j in range(len(maze[0])):
         if maze[i][j] == 'S':
             start = (i, j)
-            print("Start", start)
         if maze[i][j] == 'E':
             stop = (i, j)
-            print("Stop", stop)
-
-#print(start, stop)
 
 def solve(maze, start, stop):
-    direction = (0, -1)
+    direction = (0, 1)
     queue = [(0, start, direction)]
-    #heapq.heapify(queue)
     seen = set()
 
     while queue:
         cost, position, direction = heapq.heappop(queue)
         x, y = position
         dx, dy = direction
-        #print("Cost", cost, position, direction)
         
-        nx, ny = x + dx, y + dy
         if (x, y) == stop:
-            print("Cost", cost, (nx, ny))
+            print("Cost", cost, (x, y))
             break
         
-        if (nx, ny, dx, dy) in seen:
+        if (x, y, dx, dy) in seen:
             continue
-        seen.add((nx, ny, dx, dy))
-        
+        seen.add((x, y, dx, dy))
+
+        nx, ny = x + dx, y + dy
         if 0 <= nx < len(maze[0]) and 0 <= ny < len(maze) and maze[nx][ny] != '#':
             heapq.heappush(queue, (cost + 1, (nx, ny), (dx, dy)))
         
