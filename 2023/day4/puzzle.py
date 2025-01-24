2bc078fde2e93c4ef5d2ba67c0ad20a5c74f45bd
Dawid Węgliński <dawid.weglinski@gmail.com>
2025-01-24 23:56:22 +0100

    Solution for day4 part2

--- 2023/day4/puzzle.py
+++ 2023/day4/puzzle.py
@@ -22,4 +22,28 @@ for card in data:
                 n *= 2
     points.append(n)
 
-print(sum(points))    
+print(sum(points))
+
+# part 2
+scratchcards = [(card_number, 
+                list(map(int, winning.split())),
+                list(map(int, owned.split())))
+                for card_number, numbers in enumerate(data, 1) for numbers in [numbers.split(':')]
+                for winning, owned in [numbers[1].strip().lstrip().split('|')]]
+
+copies = defaultdict(int)
+for card_number, winning, owned in scratchcards:
+    copies[card_number] += 1
+    n = 0
+    multiplier = copies[card_number] if card_number in copies.keys() else 1
+    for number in owned:
+        if number in winning: n += 1
+    #print(n)
+    if n > 0:
+        for j in range(1, n  + 1):
+            copies[card_number + j] +=  multiplier
+
+print(sum(copies.values()))
+
+
+            
