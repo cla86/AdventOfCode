efae3e80c26389ead39efc81ad117815f3314425
Dawid Węgliński <dawid.weglinski@gmail.com>
2025-01-26 09:30:59 +0100

    Solution for part2 of day6

--- 2023/day6/puzzle.py
+++ 2023/day6/puzzle.py
@@ -9,19 +9,25 @@ times = list(map(int, re.findall(r'\d+', data[0])))
 distances = list(map(int, re.findall(r'\d+', data[1])))
 races = list(zip(times, distances))
 
+def solve(races):
+    possibilities = []
+    for time, distance in races:
+        n = 0
+        for charge in range(1, int(time)):
+            speed = charge
+            time_left = time - charge
+            max_distance = speed * time_left
+
+            if max_distance > distance:
+                n += 1
+
+        possibilities.append(n)
+    return possibilities
+
 # part 1
-possibilities = []
-for time, distance in races:
-    n = 0
-    for charge in range(1, int(time)):
-        speed = charge
-        time_left = time - charge
-        max_distance = speed * time_left
-        
-        if max_distance > distance:
-            n += 1
-    
-    possibilities.append(n)
+print(reduce(lambda x, y: x * y, solve(races)))
 
-print(reduce(lambda x, y: x * y, possibilities))
+# part 2
 
+races = [(int(''.join(map(str, times))), int(''.join(map(str, distances))))]
+print(reduce(lambda x, y: x * y, solve(races)))
