ac1018d8da0716f33386bd4d5d4105a76886e842
Dawid Węgliński <dawid.weglinski@cloudware.pl>
2025-01-27 13:07:36 +0100

    Solution for day8 part1

---
+++ 2023/day8/puzzle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+with open('input.txt', 'r') as f:
+    instructions, nodes = f.read().split('\n\n')
+
+instructions = list(instructions)
+nodes = [[elem.strip(), lr.strip()] for elem, lr in [points.split('=') for points in nodes.split('\n')]]
+nodes = {node[0]: [left.lstrip('('), right.lstrip().strip(')')] for node in nodes for left, right in [node[1].split(',')]}
+
+i = 0
+start = nodes['AAA']
+end = nodes['ZZZ']
+
+while True:
+    for direction in instructions:
+        if direction == "L": start = nodes[start[0]]
+        if direction == "R": start = nodes[start[1]]
+        #print(direction, start)
+        i += 1
+        if start == end:
+            break
+
+    if start == end:
+        break
+print(i)
