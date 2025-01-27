6ec2d2c903c43eaf9a9ccbd0a8dcba20fd9df460
Dawid Węgliński <dawid.weglinski@cloudware.pl>
2025-01-27 10:23:26 +0100

    Solution for day7 part 2

--- 2023/day7/puzzle.py
+++ 2023/day7/puzzle.py
@@ -4,16 +4,23 @@ from collections import Counter
 with open("input.txt", 'r') as f:
     data = [line.strip().split() for line in f]
 
-rank = {
-    'T': 10,
-    'J': 11,
-    'Q': 12,
-    'K': 13,
-    'A': 14
-}
+def joker(counter, cards):
+    jokers = counter['J']
+    del counter['J']
+    if jokers == 0:
+        #print(counter.most_common())
+        return classify(cards)
+    if jokers == 5:
+        return classify(cards.replace('J', 'A'))
+    else:
+        return classify(cards.replace('J', counter.most_common()[0][0]))
 
-def classify(cards):
+def classify(cards, wildcard=False):
     counter = Counter(cards)
+
+    if wildcard:
+        return joker(counter, cards)
+    
     most_common = counter.most_common()
     if most_common[0][1] == 5:
         return 7
@@ -31,11 +38,26 @@ def classify(cards):
         return 1
     return 0
 
-def score(cards):
+def score(cards, wildcard=False):
+    rank = {
+        'T': 10,
+        'J': 1 if wildcard else 11,
+        'Q': 12,
+        'K': 13,
+        'A': 14
+    }
     return [int(card) if card.isdigit() else rank[card] for card in cards]
 
 # part 1
 scored = sorted([[cards, classify(cards), score(cards), bid] for cards, bid in data], key=lambda x: (x[1], x[2]))
+total_winnings = 0
+for i, scores in enumerate(scored, 1):
+    total_winnings += i * int(scores[3])
+
+print(total_winnings)
+
+# part 2
+scored = sorted([[cards, classify(cards, wildcard=True), score(cards, wildcard=True), bid] for cards, bid in data], key=lambda x: (x[1], x[2]))
 total_winnings = 0
 for i, scores in enumerate(scored, 1):
     total_winnings += i * int(scores[3])
