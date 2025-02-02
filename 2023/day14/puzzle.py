9e567589b5790189626d2789e1aa450ac71b806e
Dawid Węgliński <dawid.weglinski@cloudware.pl>
2025-02-02 18:53:53 +0100

    Solution for day14 part 1

---
+++ 2023/day14/puzzle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+with open('input.txt', 'r') as f:
+    platform = [x.strip() for x in f.readlines()]
+    p = {}
+
+def tilt(platform):
+    sum = 0
+    for row in list(zip(*platform)):
+        l = len(row)
+        for i in range(0, len(row)):
+            if row[i] == 'O':
+                for j in reversed(range(0, i)):
+                    if row[j] == 'O':
+                        l -= 1
+                        break
+                    elif row[j] == '#':
+                        l = len(row) - 1 - j
+                        break
+                sum += l
+
+    return sum
+
+# part 1
+print(tilt(platform))
